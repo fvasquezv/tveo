0e0226b48fe25f2361186d89b01135b87e067fb3
Fabian Vasquez <vvfabiangonzalo@gmail.com>
2016-06-14 02:40:27 -0400

    Initial Commit

---
+++ tveo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tveo
+//
+//  Created by Fabian Vasquez on 14-06-16.
+//  Copyright © 2016 Fabian Vasquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
